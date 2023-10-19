e0747c682f7e741c96d383abd12c928ae1874f41
cauan <c.goes@gindumac.com>
2023-10-19 08:59:03 +0200

    feat: tailwind gindumac

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,13 @@
-import type { Config } from 'tailwindcss';
-import defaultTheme from 'tailwindcss/defaultTheme';
+/* eslint-disable @typescript-eslint/no-var-requires */
 
-export default {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {
       fontFamily: {
-        primary: ['Inter', ...defaultTheme.fontFamily.sans],
+        primary: 'var(--font-primary)',
+        secondary: 'var(--font-secondary)',
       },
       colors: {
         primary: {
@@ -21,19 +22,46 @@ export default {
           700: 'rgb(var(--tw-color-primary-700) / <alpha-value>)',
           800: 'rgb(var(--tw-color-primary-800) / <alpha-value>)',
           900: 'rgb(var(--tw-color-primary-900) / <alpha-value>)',
-          950: 'rgb(var(--tw-color-primary-950) / <alpha-value>)',
+        },
+        brand: {
+          DEFAULT: '#2F3D4F',
+          50: '#8198B4',
+          100: '#758DAD',
+          200: '#5D789C',
+          300: '#4D6582',
+          400: '#3E5169',
+          500: '#4d749b',
+          600: '#1A222C',
+          700: '#050709',
+          800: '#45546b',
+          900: '#000000',
+        },
+        secondary: {
+          500: '#F8A912',
+          800: '#F49712',
+        },
+        success: {
+          400: '#39ac73',
+          500: '#396',
+        },
+        muted: {
+          500: '#4D4D4D',
         },
         dark: '#222222',
+        'gray-light': '#F7F7F7',
+        'blue-gray': {
+          500: '#6B7280',
+        },
       },
       keyframes: {
         flicker: {
           '0%, 19.999%, 22%, 62.999%, 64%, 64.999%, 70%, 100%': {
-            opacity: '0.99',
+            opacity: 0.99,
             filter:
               'drop-shadow(0 0 1px rgba(252, 211, 77)) drop-shadow(0 0 15px rgba(245, 158, 11)) drop-shadow(0 0 1px rgba(252, 211, 77))',
           },
           '20%, 21.999%, 63%, 63.999%, 65%, 69.999%': {
-            opacity: '0.4',
+            opacity: 0.4,
             filter: 'none',
           },
         },
@@ -51,6 +79,29 @@ export default {
         shimmer: 'shimmer 1.3s linear infinite',
       },
     },
+    fontSize: {
+      xs: '.75rem',
+      sm: '.875rem',
+      base: '1rem',
+      lg: '1.125rem',
+      xl: '1.3125rem',
+      '2xl': '1.5rem',
+      '3xl': '1.875rem',
+      '4xl': '2.25rem',
+      '5xl': '3rem',
+      '6xl': '4rem',
+      '7xl': '5rem',
+    },
+    backgroundImage: {
+      'arrow-right': "url('/arrow-01.svg')",
+    },
   },
-  plugins: [require('@tailwindcss/forms')],
-} satisfies Config;
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/aspect-ratio'),
+    function ({ addVariant }) {
+      addVariant('child', '& > *');
+      addVariant('child-hover', '& > *:hover');
+    },
+  ],
+};
